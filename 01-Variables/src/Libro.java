1c403097c9c70accfce85691d356b8aab7743300
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-01-24 12:54:46 -0600

    Se agrego un archivo ejemplo del uso de variables

---
+++ 01-Variables/src/Libro.java
@@ -0,0 +1,17 @@
+public class Libro {
+    public static void main(String[] args) {
+        //Datos de un libro
+        String titulo = "El Quijote de la Mancha";
+        String autor = "Miguel de Cervantes";
+        int anio = 1605;
+        int paginas = 293;
+        boolean disponible = true;
+
+        // Impresión de datos
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Año: " + anio);
+        System.out.println("Páginas: " + paginas);
+        System.out.println("Disponbile: " + disponible);
+    }
+}
