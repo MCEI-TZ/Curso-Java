c23e2528781d19cfaf5e5e1d8ef00ade941dd6ca
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-01-24 13:09:52 -0600

    Se agrego un archivo en el que se muestra los tipos de datos en java y sus máximos y minimos de algunos de ellos

---
+++ 01-Variables/src/TipoDatos.java
@@ -0,0 +1,30 @@
+public class TipoDatos {
+    public static void main(String[] args) {
+        // Valores Maximos y minimos de cada uno
+        //*Byte
+        System.out.println("Minimo valor Byte = " + Byte.MIN_VALUE);
+        System.out.println("Máximo valor Byte = " + Byte.MAX_VALUE);
+
+        //*Short
+        System.out.println("Minimo valor short = " + Short.MIN_VALUE );
+        System.out.println("Máximo valor short = " + Short.MAX_VALUE );
+
+        //*Integer
+        System.out.println("Minimo valor integer = " + Integer.MIN_VALUE);
+        System.out.println("Máximo valor integer = " + Integer.MAX_VALUE);
+
+        //*Long
+        //! NOta: al mommento de declarar una variable con un valor long se tiene que poner una L para declarar que es long [[variable = 3.1432423L]]
+        System.out.println("Minimo valor Long = " + Long.MIN_VALUE);
+        System.out.println("Máximo valor Long = " + Long.MAX_VALUE);
+
+        //*Float
+        System.out.println("Minimo valor Float = " + Float.MIN_VALUE);
+        System.out.println("Máximo valor Float = " + Float.MAX_VALUE);
+
+        //*Double
+        //! NOta: al mommento de declarar una variable con un valor long se tiene que poner una L para declarar que es long [[variable = 3.1432423L]]
+        System.out.println("Minimo valor Double = " + Double.MIN_VALUE);
+        System.out.println("Máximo valor Double = " + Double.MAX_VALUE);
+    }
+}
