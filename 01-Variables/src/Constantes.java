dd4fc6de7759cf324d964547d8fd33d3950d38b4
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-01-24 15:51:23 -0600

    Se agrego un archivo en el que se hace uso de las constantes

---
+++ 01-Variables/src/Constantes.java
@@ -0,0 +1,16 @@
+public class Constantes {
+    public static void main(String[] args) {
+        //* Declaración de constantes
+        //* Ejemplo del uso de constantes
+        //! Nota para hacerla constante se pone un "final"
+        final double PI = 3.1416;
+        final String NOMBRE_AUTOR = "Miguel de Cervantes";
+        final int ANIO_PUBLICACION = 1605;
+
+        //* Impresión de constantes
+        System.out.println("PI = " + PI);
+        System.out.println("NOMBRE_AUTOR = " + NOMBRE_AUTOR);
+        System.out.println("ANIO_PUBLICACION = " + ANIO_PUBLICACION);
+
+    }
+}
