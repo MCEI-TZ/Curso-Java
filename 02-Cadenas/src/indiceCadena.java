731ab2397ef27c1e308f8c3031c0e88df6a11d58
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-01-24 18:28:13 -0600

    Se aplico el uso del uso de los indices en cadenas

--- 02-Cadenas/src/indiceCadena.java
+++ 02-Cadenas/src/indiceCadena.java
@@ -1,5 +1,17 @@
 public class indiceCadena {
     public static void main(String[] args) {
-        //" Manejo de Indices en cadenas
+        //! Manejo de Indices en cadenas
+
+        String cadena1 = "1 Hola Mundo 0";
+
+        var primerCaracter = cadena1.charAt(0);
+        System.out.println("primerCaracter = " + primerCaracter);
+        
+        var ultimoCaracter = cadena1.charAt(cadena1.length() -1 );
+
+        System.out.println("ultimoCaracter = " + ultimoCaracter);
+        
+        var randomCaracter = cadena1.charAt(3);
+        System.out.println("randomCaracter = " + randomCaracter);
     }
 }
