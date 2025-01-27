37d6d5a585ede8096f316faa817a150885071bfa
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-01-26 21:25:25 -0600

    Ejemplo de Sistema Empleados Se agrego un pequeño ejercicio en el que se reciben los datos de un empleado y los convertimos en su dato respectivo

---
+++ 03-ManejoConsola/src/SistemaEmpleado.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SistemaEmpleado {
+    public static void main(String[] args) {
+        // ** Sistema Sencillo para Recultar Empleados
+
+        System.out.println("Sistema para Recutlar empleados");
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre completo");
+        var nombreCompleto = scanner.nextLine();
+
+        System.out.println("Ingrese su edad");
+        var edad = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Ingrese el salario que estima ganar");
+        var salarioEstimado = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("¿Es usted jefe de departamento? reponda con true o false");
+        var esJefeDeDepartamento = Boolean.parseBoolean(scanner.nextLine());
+
+        System.out.println("Datos del Empleado \n" + "nombre: "+nombreCompleto +"\nEdad: "+edad+"\nSalario: "+salarioEstimado+"\n ¿Es jefe de departamento: "+ esJefeDeDepartamento  );
+
+    }
+}
