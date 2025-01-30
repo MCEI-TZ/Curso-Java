525c59e046003c08574b61d952fbc051fd157233
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-01-30 11:38:27 -0600

    OperadoresLogicos

---
+++ 04-Operadores/src/OperadoresLogicos.java
@@ -0,0 +1,28 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        //* Operadores Logicos
+        System.out.println("Operadores Logicos");
+        boolean var1 = false, var2 = false;
+
+        System.out.printf("var1 = %b, var2 = %b %n", var1, var2);
+
+        //? AND
+        boolean and = var1 && var2;
+        System.out.println("Resultado AND var1 && var2 = " + and);
+
+        //? OR
+        boolean or = var1 || var2;
+        System.out.println("Resultado OR var1 || var2 = " + or);
+
+        //? NOT
+        boolean not =!var1;
+        System.out.println("Resultado NOT! var1 = " + not);
+
+        //? XOR bits si son diferentes
+        //var1 = false;
+        //var2 = true;
+        boolean xor = var1 ^ var2;
+        System.out.println("Resultado XOR var1 ^ var2 = " + xor);
+
+    }
+}
