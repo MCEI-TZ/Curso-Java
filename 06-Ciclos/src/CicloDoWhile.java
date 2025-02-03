9257a1e75c3aadbeb51ef96e89808fa4c56fd68d
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-03 13:18:26 -0600

    Example of cicle while (odd numbers)

--- 06-Ciclos/src/CicloDoWhile.java
+++ 06-Ciclos/src/CicloDoWhile.java
@@ -8,5 +8,26 @@ public class CicloDoWhile {
             System.out.println("El número es: " + num);
             num++;
         } while (num <= 10);
+
+        //* EJEMPLO CICLO DO WHILE numeracion descendente de 10 a 1
+
+        System.out.println("----------------------------------------------------------------");
+        int contador = 10;
+        do {
+            System.out.println("El número es: " + contador);
+            contador--;
+        } while (contador >= 1);
+
+        //* Numeros impares
+
+        System.out.println("----------------------------------------------------------------");
+
+        int numero = 0;
+        do {
+            if (numero % 2 != 0) { //* Condicion para impar
+             System.out.println("El número impar es: " + numero);
+            }
+            numero++;
+        } while (numero <= 20);
     }
 }
