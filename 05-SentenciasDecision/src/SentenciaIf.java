24d839f6cbcb6231ce97073f98f975d4a02be0bb
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-03 08:34:55 -0600

    Sentencia if-elseif-else

--- 05-SentenciasDecision/src/SentenciaIf.java
+++ 05-SentenciasDecision/src/SentenciaIf.java
@@ -13,6 +13,19 @@ public class SentenciaIf {
         } else {
             System.out.println("Eres menor de edad");
         }
+        
+        //* Sentencia if-else else if
+
+        System.out.println("Ingrese su edad");
+        var edad1 = Integer.parseInt(scanner.nextLine());
+
+        if (edad1 >= 18) {
+            System.out.println("Eres mayor de edad");
+        } else if (edad1 >= 13 && edad1 <18) {
+            System.out.println("Eres adolecente");
+        } else {
+            System.out.println("Eres menor de edad");
+        }
 
     }
 }
