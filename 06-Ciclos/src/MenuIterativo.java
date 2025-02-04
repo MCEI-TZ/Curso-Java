bde486dc8108d8c412b101305cc6e9c23f2a741f
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-03 19:18:52 -0600

    Add MenuIterativo.java

---
+++ 06-Ciclos/src/MenuIterativo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MenuIterativo {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("*** Sistema de Administracion de Cuentas ***");
+        var salida = false;
+
+        while(!salida){
+            System.out.println("""
+                    Seleccione una opcion:
+                    1. Crear Cuenta
+                    2. Modificar Cuenta
+                    3. Eliminar Cuenta
+                    4. Salir
+                """);
+            System.out.println(" Opcion: ");
+
+            var opcion = Integer.parseInt(scanner.nextLine());
+
+            switch(opcion){
+                case 1 -> System.out.println("Creando cuenta ...");
+                case 2 -> System.out.println("Modificando cuenta ...");
+                case 3 -> System.out.println("Eliminando cuenta ...");
+                case 4 -> salida = true;
+            }
+        }
+    }
+}
