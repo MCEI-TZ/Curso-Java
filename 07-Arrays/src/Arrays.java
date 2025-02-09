ac72bfbfb68b72db51696a9e9dfe8da97cfbf3f7
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-08 19:40:50 -0600

    Introduction to Arrays

---
+++ 07-Arrays/src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        //* Declare and initialize an array of integers
+        int[] numbers;
+
+        //* Assign values to the array elements
+        numbers = new int[3];
+
+    }
+}
