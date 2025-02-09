208003da4d823ee73159f914179afbb177f840c6
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-08 19:43:20 -0600

    Update Arrays.java

--- 07-Arrays/src/Arrays.java
+++ 07-Arrays/src/Arrays.java
@@ -7,5 +7,15 @@ public class Arrays {
         //* Assign values to the array elements
         numbers = new int[3];
 
+        //* Accessing and modifying array elements
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+
+        System.out.println("Original array elements:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
     }
 }
