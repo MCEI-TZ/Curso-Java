ef1faac4dcd5dce66ac2127a233c9ae2cae57018
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-10 13:06:29 -0600

    Add Persona.java

---
+++ 10-Class/src/Persona.java
@@ -0,0 +1,16 @@
+//* Define the class
+public class Persona {
+    //* Define the atributes
+    private String name;
+    private String LastName;
+    private String email;
+    private long phoneNumber;
+
+    //* Define the function
+    void showPerson(){
+        System.out.println("Name: " + name);
+        System.out.println("Last Name: " + LastName);
+        System.out.println("Email: " + email);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
