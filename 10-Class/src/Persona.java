b8fad27c0edc1cfb67dd69c3f911377c840316f6
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-10 13:15:23 -0600

    Add Persona.java

--- 10-Class/src/Persona.java
+++ 10-Class/src/Persona.java
@@ -4,7 +4,7 @@ public class Persona {
     private String name;
     private String LastName;
     private String email;
-    private long phoneNumber;
+    private int phoneNumber;
 
     //* Define the function
     void showPerson(){
@@ -12,5 +12,26 @@ public class Persona {
         System.out.println("Last Name: " + LastName);
         System.out.println("Email: " + email);
         System.out.println("Phone Number: " + phoneNumber);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Creation of Object Persona");
+        var person1 = new Persona();
+        //* Create a new Persona
+        person1.name = "John";
+        person1.LastName = "Doe";
+        person1.email = "johndoe@example.com";
+        person1.phoneNumber = 1234567890;
+        //* Show the details of the person1
+        person1.showPerson();
+
+        //* Create a new person
+        var persona2 = new Persona();
+        persona2.name = "Jane";
+        persona2.LastName = "Smith";
+        persona2.email = "janesmith@example.com";
+        persona2.phoneNumber = 1876543210;
+        //* Show the details of the persona2
+        persona2.showPerson();
     }
 }
