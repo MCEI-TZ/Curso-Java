044836e421dcc7050575e6c81c12969865b2502b
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-11 18:22:03 -0600

    Add ForEach.java

---
+++ 11-AdvancedTopics/src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int ages [] = {1, 2, 3, 4, 5};
+
+        for (var age : ages){
+            System.out.print(" Age: "+ age); //prints each number individually
+        }
+    }
+}
