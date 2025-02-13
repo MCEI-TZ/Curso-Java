339387ec1898f01687712ebb572b7680c4d0dc1b
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-13 12:07:45 -0600

    Section Management Files Update WriteFile.java

--- 12-ManagementFiles/src/files/WriteFile.java
+++ 12-ManagementFiles/src/files/WriteFile.java
@@ -7,14 +7,13 @@ import java.io.PrintWriter;
 public class WriteFile {
     public static void main(String[] args) {
         //* Write file
-        boolean isAdd = false;
         var fileName = "miFIle.txt";
         var file = new File(fileName);
+        boolean isAdd = file.exists();
         try {
             if (!file.exists()) {
                 System.out.println("The file exits!");
             }
-            isAdd = file.exists();
             var output = new PrintWriter(new FileWriter(file, isAdd));
             output.write("This is a new line.\n");
             output.write("This is another new line.\n");
