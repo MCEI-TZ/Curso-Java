447d91120c4842cb021e5761d165dac0c6b34c40
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-22 09:08:36 -0600

    perf(main): Add main method
    - Make a proof of listPeope() of PersonaDAO
    
    Author: MCEI-TZ

--- 15-Hibernate_Operations/src/main/java/main/HibernatePU.java
+++ 15-Hibernate_Operations/src/main/java/main/HibernatePU.java
@@ -1,5 +1,6 @@
 package main;
 
+import data.PersonaDAO;
 import domain.Persona;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -10,14 +11,8 @@ import java.util.List;
 
 public class HibernatePU {
     public static void main(String[] args) {
-        String hql = "SELECT p FROM Persona p";
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("HibernateExample1");
-        EntityManager entityManager = factory.createEntityManager();
-
-        Query query = entityManager.createQuery(hql);
-        List<Persona> personas = query.getResultList();
-        for (Persona persona : personas) {
-            System.out.println(persona);
-        }
+        var person = new PersonaDAO();
+        //* List people
+        person.listPeople();
     }
 }
