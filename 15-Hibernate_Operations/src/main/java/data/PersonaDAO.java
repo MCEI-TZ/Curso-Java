f10f1706227296c2fdc9ed7d4492541524c6afaf
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-02-22 09:05:56 -0600

    feat(models): Add PersonaDAO class
    - Add a no-argument constructor that initializes the EntityManagerFactory and EntityManager.
    - Add listPeople() method that retrieves and prints a list of Persona objects from the persona table.
    
    Author: MCEI-TZ

---
+++ 15-Hibernate_Operations/src/main/java/data/PersonaDAO.java
@@ -0,0 +1,25 @@
+package data;
+
+import domain.Persona;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class PersonaDAO {
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    public PersonaDAO(){
+        emf= Persistence.createEntityManagerFactory("HibernatePU");
+        em = emf.createEntityManager();
+    }
+
+    public void listPeople(){
+        String hql = "SELECT p FROM Persona p";
+        Query query = em.createQuery(hql);
+        List<Persona> personas = query.getResultList();
+        for(Persona p: personas){
+            System.out.println(p);
+        }
+    }
+}
