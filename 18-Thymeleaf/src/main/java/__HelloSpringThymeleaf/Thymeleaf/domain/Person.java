ed2bad781520acca0b80311a90d69cb1cffbc36d
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-05 13:34:00 -0600

    feat(entity): Add entity Person ✨

---
+++ 18-Thymeleaf/src/main/java/__HelloSpringThymeleaf/Thymeleaf/domain/Person.java
@@ -0,0 +1,11 @@
+package __HelloSpringThymeleaf.Thymeleaf.domain;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+}
