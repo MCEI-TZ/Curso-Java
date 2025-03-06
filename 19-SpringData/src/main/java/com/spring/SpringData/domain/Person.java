2ea8128dd5c1d85ba77bd2c841c82b4c1b0946ab
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-05 21:24:32 -0600

    feat(models): Add entity Person ✨

--- 19-SpringData/src/main/java/com/spring/SpringData/domain/Person.java
+++ 19-SpringData/src/main/java/com/spring/SpringData/domain/Person.java
@@ -1,11 +1,26 @@
 package com.spring.SpringData.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class Person {
+@Entity
+@Table(name = "persona")
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_persona")
+    private Long idPerson;
+
     private String name;
-    private String lastName;
+
+    private String lastname;
+
     private String email;
+
     private String phone;
 }
