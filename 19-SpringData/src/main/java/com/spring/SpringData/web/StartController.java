929195529cfd6e4290c388b3d26a077880f63cda
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-06 08:01:58 -0600

    feat(controller): Add controllers for get and post for person

--- 19-SpringData/src/main/java/com/spring/SpringData/web/StartController.java
+++ 19-SpringData/src/main/java/com/spring/SpringData/web/StartController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -22,5 +23,16 @@ public class StartController {
         var people = personService.listPeople();
         model.addAttribute("people", people);
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String addPerson(Person person) {
+        return "modify";
+    }
+
+    @PostMapping("/save")
+    public String savePerson(Person person) {
+        personService.savePerson(person);
+        return "redirect:/";
     }
 }
