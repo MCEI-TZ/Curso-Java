2264feccbf8f47801ba7b2df8898aa713d584cd1
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-06 08:37:02 -0600

    feat(controller): Add controller for delete person

--- 19-SpringData/src/main/java/com/spring/SpringData/web/StartController.java
+++ 19-SpringData/src/main/java/com/spring/SpringData/web/StartController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -41,5 +42,11 @@ public class StartController {
         var personToEdit = personService.findPerson(person);
         model.addAttribute("person", personToEdit);
         return "modify";
+    }
+
+    @GetMapping("/delete/{idPerson}")
+    public String deletePerson(Person person) {
+        personService.deletePerson(person);
+        return "redirect:/";
     }
 }
