8be2c9104d454c7a73505a30e39360a88634426b
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-06 11:13:17 -0600

    perf(controller): Catch error with SpringValidation ⚗️

--- 19-SpringData/src/main/java/com/spring/SpringData/web/StartController.java
+++ 19-SpringData/src/main/java/com/spring/SpringData/web/StartController.java
@@ -3,10 +3,12 @@ package com.spring.SpringData.web;
 import com.spring.SpringData.dao.IPersonDAO;
 import com.spring.SpringData.domain.Person;
 import com.spring.SpringData.services.PersonService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +34,10 @@ public class StartController {
     }
 
     @PostMapping("/save")
-    public String savePerson(Person person) {
+    public String savePerson(@Valid Person person, Errors errors) {
+        if(errors.hasErrors()){
+            return "modify";
+        }
         personService.savePerson(person);
         return "redirect:/";
     }
