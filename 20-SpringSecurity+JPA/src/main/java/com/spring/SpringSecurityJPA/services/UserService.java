e4f1a072662e9b2ff983cd7457eea03adf2138e7
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-11 13:55:41 -0600

    perf(service): Add annotation @Transactional for Spring ⚡

--- 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/services/UserService.java
+++ 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/services/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
@@ -23,6 +24,7 @@ public class UserService implements UserDetailsService {
     private IUserDAO iUserDAO;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserProfile userProfile = iUserDAO.findByUsername(username);
 
