6923c3d7dedd8e5118ea4b813681f1f5fb72d73b
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-11 13:53:47 -0600

    perf(models): Rename the tables from DB for a better integration ✨

--- 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/domain/Rol.java
+++ 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/domain/Rol.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 
 @Entity
 @Data
-@Table(name = "Roles")
+@Table(name = "roles")
 public class Rol implements Serializable {
     private static final long serialVersionUID = 1L;
 
--- 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/domain/UserProfile.java
+++ 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/domain/UserProfile.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 
 @Entity
-@Table(name="User")
+@Table(name="user")
 @Data
 public class UserProfile implements Serializable {
     private static final long serialVersionUID = 1L;
