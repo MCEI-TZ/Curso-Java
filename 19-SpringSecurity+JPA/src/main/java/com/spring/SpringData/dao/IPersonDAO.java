22a3b2a0e8d86d4f7c04d242834ded6da3515e2d
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-11 09:57:09 -0600

    Removed files

--- 19-SpringSecurity+JPA/src/main/java/com/spring/SpringData/dao/IPersonDAO.java
+++
@@ -1,8 +0,0 @@
-package com.spring.SpringData.dao;
-
-
-import com.spring.SpringData.domain.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IPersonDAO extends CrudRepository<Person, Long> {
-}
