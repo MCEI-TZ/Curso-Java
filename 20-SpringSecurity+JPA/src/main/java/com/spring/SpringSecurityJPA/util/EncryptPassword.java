e4d6086c17f6c165cb8a031fea569d64c7679d64
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-03-11 13:57:41 -0600

    feat: Take another password for be test in the database 🍻

--- 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/util/EncryptPassword.java
+++ 20-SpringSecurity+JPA/src/main/java/com/spring/SpringSecurityJPA/util/EncryptPassword.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class EncryptPassword {
     public static void main(String[] args) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        var password = "123";
+        var password = "Mexz1310@20001";
         var encodedPassword = encodePassword(password);
         System.out.println("Encoded password: " + encodedPassword);
     }
